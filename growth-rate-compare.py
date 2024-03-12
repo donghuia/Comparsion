e731b711b10f121f14213aa072e15b61276966b6
donghuia <153625521+donghuia@users.noreply.github.com>
2024-03-12 11:05:56 +0800

    Update growth-rate-compare.py

--- growth-rate-compare.py
+++ growth-rate-compare.py
@@ -15,12 +15,16 @@ def w(n,k):
 
 for n in range(2,9):
     for k in range(2,9):
-        a = E(n,k)/E(n-1,k)-1
-        b = (n*k*(n*k+pow(2,k)))/((n-1)*k*((n-1)*k+pow(2,k)))-1
-        print(a)
-        print(b)
+        a1 = E(n,k)/E(n-1,k)-1
+        b1 = (n*k*(n*k+pow(2,k)))/((n-1)*k*((n-1)*k+pow(2,k)))-1
+        a2 = E(n, k) / E(n, k-1) - 1
+        b2 = (n * k * (n * k + pow(2, k))) / (n * (k-1) * (n * (k-1) + pow(2, k-1))) - 1
+        print(a1)
+        print(b1)
+        print(a2)
+        print(b2)
         print("n="+str(n)+", k="+str(k)+":")
-        if a>b:
+        if a1>b1 and a2>b2:
             print("The algorithm DPC is sublinear!")
         else:
             print("The algorithm DPC is not sublinear!")
